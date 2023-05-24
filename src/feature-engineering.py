aab742967f568264bb65709cb8a5d8018bb9576f
Luning-Yang <56569117+Luning-Yang@users.noreply.github.com>
2023-05-23 19:50:14 -0700

    feature

--- src/feature-engineering.py
+++ src/feature-engineering.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime
+from sklearn.impute import SimpleImputer
 
 # Over every single 
 def polyline_to_trip_duration(polyline):
@@ -26,8 +27,17 @@ if __name__ == "__main__":
 
 	# One-hot encoding CALL_TYPE
 	df_encoded_call_type = pd.get_dummies(df_tr['CALL_TYPE'], prefix='call_type')
+	# One-hot encoding TAXI_ID
+	df_encoded_taxi_id = pd.get_dummies(df_tr['TAXI_ID'], prefix='taxi_id')
+
+	# impute and One-hot encoding ORIGIN_STAND
+	imputer = SimpleImputer(strategy='most_frequent')
+	df_tr['ORIGIN_STAND'] = imputer.fit_transform(df_tr[['ORIGIN_STAND']])
+	df_encoded_origin_stand = pd.get_dummies(df_tr['ORIGIN_STAND'], prefix='origin_stand')
+
+
 	# Join the encoded dataframes back with the original dataframe
-	df_tr = pd.concat([df_tr, df_encoded_call_type], axis=1)
+	df_tr = pd.concat([df_tr, df_encoded_call_type, df_encoded_taxi_id, df_encoded_origin_stand], axis=1)
 	df_tr = df_tr.drop(['TRIP_ID', 'CALL_TYPE', 'ORIGIN_CALL', 
 	                    'ORIGIN_STAND', 'TAXI_ID', 'TIMESTAMP', 
 	                    'DAY_TYPE', 'MISSING_DATA', 'POLYLINE'], axis=1)
